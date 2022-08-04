0a39305f55a41e33c992f7510579474b4caccacf
HenriqueBurle33 <110400696+HenriqueBurle33@users.noreply.github.com>
2022-08-04 13:01:19 -0300

    Update script.js

--- SEMANA_01/02_ATIVIDADES/script.js
+++ SEMANA_01/02_ATIVIDADES/script.js
@@ -1,53 +1,80 @@
-/**
- * Inteli - Instituto de Tecnologia e Liderança
- * Aplicativo para introdução à lógica e programação
- * 
- * Esse aplicativo é um jogo (game) do tipo "Flappy Bird".
- * Ver mais em https://pt.wikipedia.org/wiki/Flappy_Bird
- * 
- * O jogador usa as teclas para cima e para baixo para fazer o dragão passar por entre as colunas.
- * Cada vez que o dragão passa, o jogador ganha um ponto.
- * O jogo se encerra quando o dragão bate em alguma coluna ou no chão.
- * 
- * Esse é um código-fonte aberto, desenvolvido com fins educacionais.
- * Pode ser usado e alterado livremente.
- * 
- * Julho/2021
- */
+extends Node # instancia a classe Node2D
+
+var status = 1 # 1 jogando, 0 parado
+var vscore = 0 # pontuação obtida
+var speed = 5 # velocidade, aumente esse valor para deixar o jogo mais difícil
+var gravity = 1.5 # gravidade, aumente esse valor para deixar o jogo mais difícil
+
+# executa essa função ao carregar o jogo
+func _ready():
+	# oculta o "gameover"
+	$gameover.hide()
 
 
-/*** Contexto **********************************/
-
-// Canvas 
-// Esse é o local onde aparece o jogo
-// No arquivo index.html, está contido em <canvas></canvas>
-// Ver mais em https://www.w3schools.com/html/html5_canvas.asp
-var canvas  = document.getElementById('canvas');
-var context = canvas.getContext('2d');
-
-
-
-/*** Iniciar o jogo **********************************/
-
-Jogar();
-
-
-
-/*** Funções **********************************/
-
-/**
- * Roda um loop redesenhando a tela a cada volta (frame)
- */
-function Jogar() {
+# executa essa função a cada frame (60 FPS)
+func _process(delta):
 	
-	
-	// limpa o canvas
-	context.clearRect(0, 0, canvas.width, canvas.height);
-	
-	
-	// essa chamada faz a função ficar em loop
-	requestAnimationFrame(Jogar); 
-}
+	if status == 1: # jogando
+		
+		# movimenta o cenário do fundo
+		$background.position.x -= 1*speed
+		if ($background.position.x) < -200:
+			$background.position.x = 600
+			
+		# movimenta as colunas para colisão
+		$columns.position.x -= 2*speed
+		if ($columns.position.x) < -550:
+			$columns.position.x = rand_range(0, 350) - 50
+			$columns.position.y = rand_range(0, 400) - 200
+		
+		# puxa o dragão para baixo
+		$dragon.position.y += gravity
+
+		# se bateu no fundo, não desce mais e termina o jogo
+		if $dragon.position.y > 480:
+			$dragon.position.y = 480
+			status = 0 # muda o status para "parado"
+
+		# se bateu no teto, não sobe mais
+		if $dragon.position.y < -20:
+			$dragon.position.y = -20
+			
+		# se apertou seta para baixo, aumenta o valor de y (posição vertical) do dragão
+		if Input.is_action_pressed("ui_down"):
+			$dragon.position.y += 2
+
+		# se apertou seta para cima, diminui o valor de y (posição vertical) do dragão
+		if Input.is_action_pressed("ui_up"):
+			$dragon.position.y -= 4
+			
+	elif status == 0: # parado
+		
+		$dragon/dragonImages.playing = false # faz dragão parar de bater as asas
+		$gameover.show() # exibe imagem gameover
+
+		# se apertou enter ou space, recomeça o jogo
+		if Input.is_action_pressed("ui_accept"):
+			$score.set_text("0") # zera o score
+			vscore = 0 # zera o score
+			status = 1 # muda o status para "jogando"
+			$dragon/dragonImages.playing = true # faz dragão voltar a bater as asas
+			$dragon.position.y = 0 # volta o dragão para a posição original
+			$columns.position.x = 400 # muda a posição das colunas
+			$gameover.hide() # oculta o gameover
+
+			
+
+# executa essa função quando o dragão bate na coluna
+func _on_columns_body_shape_entered(body_id, body, body_shape, local_shape):
+	if (local_shape < 2): # esse node tem 3 shapes de colisão: 0 e 1 são as colunas
+		status = 0 # muda o status para "parado"
+
+# executa essa função quando o dragão atravessa entre as colunas
+func _on_columns_body_shape_exited(body_id, body, body_shape, local_shape):
+	if (local_shape == 2): # esse node tem 3 shapes de colisão: 0 e 1 são as colunas
+		vscore += 1 # aumenta o score
+		$score.set_text(str(vscore)) # atualiza o painel
+		
 
 
 
